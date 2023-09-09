74b816791e9089ad2d3414c59e60efccdd6afb66
gabrielg08 <68970721+gabrielg08@users.noreply.github.com>
2023-09-09 16:42:17 -0400

    Add files via upload

---
+++ E-Commerse/assests/js/helpers/getProducts.js
@@ -0,0 +1,15 @@
+async function getProducts () {
+
+    try{
+        const res = await window.fetch('https://services-academlo-shopping.onrender.com')
+        const data = await res.json()
+        return data
+    }
+    catch(error){
+        console.log(error)
+    }
+
+
+}
+
+export default getProducts
